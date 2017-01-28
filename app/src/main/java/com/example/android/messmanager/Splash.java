3a430de5104d21371b2630629d85d59f19d5daaf
ASIF008 <ankitkat88@gmail.com>
2017-01-28 20:30:50 +0600

    Initial commit

---
+++ app/src/main/java/com/example/android/messmanager/Splash.java
@@ -0,0 +1,45 @@
+package com.example.android.messmanager;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+/**
+ * Created by asif on 1/28/17.
+ */
+
+public class Splash extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        final ImageView imageView = (ImageView) findViewById(R.id.logoImageView);
+        final Animation animation = AnimationUtils.loadAnimation(getBaseContext(), R.anim.rotate);
+        final Animation animationFadeOut = AnimationUtils.loadAnimation(getBaseContext(),R.anim.abc_fade_out);
+        imageView.startAnimation(animation);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                imageView.startAnimation(animationFadeOut);
+                finish();
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
